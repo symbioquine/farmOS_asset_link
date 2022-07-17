1fd08e61fad63699e98e70b40943c5710b1065b4
Symbioquine <symbioquine@gmail.com>
2022-07-16 20:00:06 -0700

    WIP: Working core/pwa module federation

--- assetlink-core/src/components/Content.vue
+++ assetlink-core/src/components/Content.vue
@@ -1,8 +1,13 @@
 <template>
-  <div style="color: red;">{{ title }}</div>
+  <div><span style="color: red;">{{ title }}</span><q-btn>Hi</q-btn></div>
 </template>
 <script>
+import { QBtn } from 'quasar';
+
 export default {
+  components: {
+    QBtn,
+  },
   data() {
     return {
       title: "Remote Component in Action..",
