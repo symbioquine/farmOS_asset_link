132eefb815e6e43d295fa55faae250d347b4c2e3
Symbioquine <symbioquine@gmail.com>
2023-02-08 09:43:10 -0800

    Expose jmespath for import by plugins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Expose jmespath for import by plugins
+
 ### Changed
 
 - Make updates non-blocking with the remote when offline
--- alinkjs/packages/assetlink/src/pluginModuleLibrary.js
+++ alinkjs/packages/assetlink/src/pluginModuleLibrary.js
@@ -4,6 +4,7 @@ import * as Quasar from 'quasar';
 import * as AssetlinkPluginApi from 'assetlink-plugin-api';
 import ngeohash from 'ngeohash';
 import haversineDistance from 'haversine-distance';
+import jmespath from 'jmespath';
 
 const libraries = {
   'assetlink-plugin-api': AssetlinkPluginApi,
@@ -13,6 +14,8 @@ const libraries = {
 
   ngeohash,
   'haversine-distance': haversineDistance,
+
+  jmespath,
 
   // TODO: Figure out how to make loading these cleaner/on-demand
   // 'vue-codemirror': import('vue-codemirror'),
